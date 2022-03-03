2f62e0b3320e7ec58e5e4aa8b02a5d1b9364a70d
Stéphane Roucheray <stephane.roucheray@perfect-memory.com>
2022-03-03 21:53:34 +0100

    feat: setup storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,28 @@
+import { StorybookConfig } from '@storybook/core-common';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-scss',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+        transcludeMarkdown: true,
+      },
+    },
+    '@storybook/addon-controls',
+  ],
+  framework: '@storybook/angular',
+  staticDirs: ['./public'],
+  core: {
+    builder: 'webpack5',
+  },
+};
+
+module.exports = config;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,19 @@
-import { setCompodocJson } from "@storybook/addon-docs/angular";
-import docJson from "../documentation.json";
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
 setCompodocJson(docJson);
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
+  options: {
+    storySort: {
+      order: ['Context Menu', ['Introduction', 'Installation and setup', 'API', 'FAQ', 'Changelog', 'Demo']],
+    },
+  },
   docs: { inlineStories: true },
-}
+};
